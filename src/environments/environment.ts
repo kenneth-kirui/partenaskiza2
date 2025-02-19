10749f29f0c282c0aedcdb0dec11acf0b9dc2a45
kenneth <kiruikenth@gmail.com>
2025-02-19 11:00:33 +0300

    changed production environment2

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,14 +1,8 @@
 export const environment = {
-  production: false,
+  production: true,
   apiUrl: 'https://parte.co.ke:8085/tunes/',
-  smsProfileCode: '1703051',
-  smsApiKey:'N2Q2ZjNlMjc3ZTBhN2ZkY0lELWFiZWExNmY2OTQ0OTRhODY5ZDkyNjc5NmRmNzQ1Yzc1',
-  smsUrl: 'https://sms.crossgatesolutions.com:18095/v1/bulksms/messages',
-  
-  safaricomAuthorization: "https://api.safaricom.co.ke/oauth2/v1/generate?grant_type=client_credentials",
-  username: "gOu9yNhm4CcJjknQlfx9bbJo6ApJL7kfAmHTHydXz5G2qRP4",
-  password: "kbSyrazeBXqwGAx2VmeJ7xpW9jEe4MtpZlZxEBoFERMWR8dv8idksGVUG3GFRllF",
-  subscribeUrl:'http://127.0.0.1:8000/subscribe/'
+  smsUrl: 'https://parte.co.ke:8085/sms/',
+  subscribeUrl:'https://parte.co.ke:8085/subscribe/'
 
   
 };
